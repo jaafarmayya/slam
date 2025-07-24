f70806a3983f14ae23750994037dd0e57f973501
Jaafar Mayya <jaafar.mayya@outlook.com>
2025-07-24 13:41:43 -0400

    rviz-config

--- src/dynamic_slam/launch/world_launch.py
+++ src/dynamic_slam/launch/world_launch.py
@@ -6,10 +6,10 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-
 def generate_launch_description():
     dy = get_package_share_directory('dynamic_slam')
-    tb3_pkg = get_package_share_directory('turtlebot3_gazebo')
+    slam_toolbox_pkg = get_package_share_directory('slam_toolbox')
+
     world_file = os.path.join(dy, 'worlds', 'turtlebot3_world.world')
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -24,12 +24,30 @@ def generate_launch_description():
         }.items()
     )
 
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(slam_toolbox_pkg, 'launch', 'online_async_launch.py')
+        ),
+        launch_arguments={
+            'use_sim_time': 'true'
+        }.items()
+    )
+
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(dy, 'launch', 'rviz2.launch.py')
+        ),
+        launch_arguments={
+            'use_sim_time': 'true'
+        }.items()
+    )
+
     lidar_subscriber = Node(
         package='dynamic_slam',
         executable='lidar_subscriber.py',
         name='lidar_subscriber',
         output='screen',
-        emulate_tty=True,                
+        emulate_tty=True,
         arguments=['--ros-args', '--log-level', 'dynamic_slam:=DEBUG'],
     )
     scan_static_filter = Node(
@@ -37,44 +55,20 @@ def generate_launch_description():
         executable='scan_static_filter.py',
         name='scan_static_filter',
         output='screen',
-        emulate_tty=True,                
+        emulate_tty=True,
         arguments=['--ros-args', '--log-level', 'dynamic_slam:=DEBUG'],
     )
 
-    # slam_image_subscriber = Node(
-    #     package='dynamic_slam',
-    #     executable='slam_image_subscriber.py',
-    #     name='slam_image_subscriber',
-    #     output='screen',
-    #     parameters=[{'use_sim_time': True}],
-    # )
-
-    teleop_cmd = [
-        'bash', '-lc',
-        'export TURTLEBOT3_MODEL=burger && ' 
-        'ros2 run turtlebot3_teleop teleop_keyboard'
-    ]
-    teleop_window = ExecuteProcess(
-        cmd=['xterm', '-hold', '-e'] + teleop_cmd,
-        output='screen',
-    )
-    
     spawn_box = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
-            '-file',
-            os.path.join(
-              get_package_share_directory('dynamic_slam'),
-              'models', 'moving_box.sdf'
-            ),
+            '-file', os.path.join(dy, 'models', 'moving_box.sdf'),
             '-entity', 'moving_box',
             '-x', '0', '-y', '1', '-z', '0'
         ],
         output='screen'
     )
-
-
 
     move_box_node = Node(
         package='dynamic_slam',
@@ -83,11 +77,22 @@ def generate_launch_description():
         output='screen',
     )
 
+    teleop_cmd = [
+        'bash', '-lc',
+        'export TURTLEBOT3_MODEL=burger && ros2 run turtlebot3_teleop teleop_keyboard'
+    ]
+    teleop_window = ExecuteProcess(
+        cmd=['xterm', '-hold', '-e'] + teleop_cmd,
+        output='screen',
+    )
+
     return LaunchDescription([
         gazebo_launch,
-        spawn_box,        
+        spawn_box,
         lidar_subscriber,
-        teleop_window,
         scan_static_filter,
-        move_box_node
+        move_box_node,
+        teleop_window,
+        slam_toolbox_launch,
+        rviz_launch,
     ])
